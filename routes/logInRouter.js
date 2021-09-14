c89ccdf24528763196708035ae50f20ad88b4f18
konsbe <kons.be@outlook.com>
2021-09-14 16:05:37 +0300

    log in with username

--- routes/logInRouter.js
+++ routes/logInRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
@@ -9,5 +10,12 @@ router.get('/', catchAsync( async(req, res)=> {
     res.locals.title = "Log In";
     res.render('loginSignin/logIn')
 }))
+
+router.post('/', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), catchAsync(async (req, res) => {
+    req.flash('success', 'You have succesfully log in');
+    res.redirect('/kitesurf')
+}))
+
+
 
 module.exports = router;
